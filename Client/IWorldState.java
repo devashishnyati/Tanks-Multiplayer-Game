fcbfd0d1045fe5947695c824eabd3f3ae7f5a400
manish0338 <manish0338@gmail.com>
2018-11-30 16:48:27 -0800

    delete files

--- Client/IWorldState.java
+++
@@ -1,6 +0,0 @@
-public interface IWorldState  
-{
-    public abstract void doOngoingGame();
-    public abstract void doPause();
-    public abstract void doGameOver();   
-}
--- server/MediatorServer.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-import java.net.SocketAddress;
-
-public interface MediatorServer {
-	byte[] getFirePacket(FirePacket packet) throws IOException;
-	byte[] getXY(XY packet,SocketAddress address) throws IOException;
-}
